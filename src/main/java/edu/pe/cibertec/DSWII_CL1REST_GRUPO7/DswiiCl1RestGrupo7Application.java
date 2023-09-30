c9486bf1e6543cfd277b9509da3ee931d0f3da75
CesarLeiva26 <cesaremanuelleivajuro@gmail.com>
2023-09-30 01:42:20 -0500

    Subiendo DSWII_CL1REST_GRUPO7

---
+++ src/main/java/edu/pe/cibertec/DSWII_CL1REST_GRUPO7/DswiiCl1RestGrupo7Application.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.DSWII_CL1REST_GRUPO7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl1RestGrupo7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl1RestGrupo7Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/pe/cibertec/DSWII_CL1REST_GRUPO7/DswiiCl1RestGrupo7ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.DSWII_CL1REST_GRUPO7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiCl1RestGrupo7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
